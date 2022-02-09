6927cefd20c6efef1e19297250a4c0d593be9fce
unknown <kgeluza@mail.ru>
2022-02-09 20:33:03 +0400

    commit 09/02/22

--- src/js/points.js
+++ src/js/points.js
@@ -1,20 +1,28 @@
-export default function addPoints() {
+function addPoints() {
   const cellsArr = Array.from(document.getElementsByClassName('cell'));
   const pointsCount = document.getElementById('count_points');
   const losingCount = document.getElementById('count_losing');
   for (let i = 0; i < cellsArr.length; i += 1) {
-    const cell = document.getElementById(`cell${i}`);
-    cell.addEventListener('click', () => {
-      if (cell.classList.contains('cell_with_char')) {
+    const cell = Array.from(document.getElementsByClassName('cell'));
+    cell[i].addEventListener('click', () => {
+      if (cell[i].classList.contains('cell_with_char')) {
         pointsCount.textContent = Number(pointsCount.textContent) + 1;
-        cell.classList.remove('cell_with_char');
+        if(Number(pointsCount.textContent) === 15) {
+          alert('Вы победили!');
+          pointsCount.textContent = 0;
+          losingCount.textContent = 0;
+      }
       } else {
         losingCount.textContent = Number(losingCount.textContent) + 1;
-      }
-
-      if (losingCount.textContent >= 5) {
-        alert('Вы проиграли!');
+        if (Number(losingCount.textContent) === 5) {
+          alert('Игра завершена! Вы проиграли');
+          pointsCount.textContent = 0;
+          losingCount.textContent = 0;
+        }
       }
     });
   }
 }
+
+
+addPoints();
